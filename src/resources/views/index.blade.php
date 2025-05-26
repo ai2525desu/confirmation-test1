3fddb99624ef8ea4f919fa3a1d8699c1870c452c
ai2525desu <umi08062525@gmail.com>
2025-05-27 03:45:52 +0900

    index select残して他OK

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,4 +1,17 @@
+<?php
+// お問い合わせの種類の部分に通じる
+$categories = ['問い合わせの種類１', '問い合わせの種類２'];
+
+foreach ($categories as $category) {
+    echo $category;
+}
+?>
+
 @extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
 
 @section('content')
 <div class="contact-form__content">
@@ -14,11 +27,18 @@
             </div>
             <!-- 姓と氏名分かれて別枠のため、divで区切ってflex予定 -->
             <div class="form__group-content">
-                <div class="form__input--last_name">
-                    <input type="text" name="last_name" placeholder="例:山田" value="">
+                <div class="form__input--name">
+                    <div class="form__input--last_name">
+                        <input type="text" name="last_name" placeholder="例:山田" value="{{ old('last_name') }}">
+                    </div>
+                    <div class="form__input--first_name">
+                        <input type="text" name="first_name" placeholder="例:太郎" value="{{ old('first_name') }}">
+                    </div>
                 </div>
-                <div class="form__input--first_name">
-                    <input type="text" name="first_name" placeholder="例:太郎" value="">
+                <div class="form__error">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
                 </div>
             </div>
         </div>
@@ -29,11 +49,125 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--radio">
-                    <input type="radio" name="gender" value="男性" checked>男性
-                    <input type="radio" name="gender" value="女性">女性
-                    <input type="radio" name="gender" value="その他">その他
+                    <input type="radio" name="gender" value="男性" checked><label>男性</label>
+                    <input type="radio" name="gender" value="女性"><label>女性</label>
+                    <input type="radio" name="gender" value="その他"><label>その他</label>
+                </div>
+                <div class="form__error">
+                    @error('gender')
+                    {{ $message }}
+                    @enderror
                 </div>
             </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="例:test@example.com" value="{{ old('email') }}">
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--tel">
+                    <input type="tel" name="tel1" placeholder="080" value="{{ old('tel1') }}">
+                    <div>-</div>
+                    <input type="tel" name="tel2" placeholder="1234" value="{{ old('tel2') }}">
+                    <div>-</div>
+                    <input type="tel" name="tel3" placeholder="5678" value="{{ old('tel3') }}">
+                </div>
+                <div class="form__error">
+                    @error('tel')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address" placeholder="例:東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('address') }}">
+                </div>
+                <div class="form__error">
+                    @error('address')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="building" placeholder="例:千駄ヶ谷マンション101" value="{{ old('building') }}">
+                </div>
+                <div class="form__error">
+                    @error('building')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせの種類</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--category">
+                    <!-- クラス名、わかりにくいか？のちにname追加予定 -->
+                    <select class="form__input--select" name="">
+                        <option value="" disabled selected style="display:none">選択してください</option>
+                        @foreach ($categories as $category)
+                        <!-- 05262233：phpにて定義した物を表示中、リレーション後category_idとnameに変更予定 -->
+                        <option value="<?php echo $category; ?>"><?php echo $category; ?></option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form__error">
+                    @error('category')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--textarea">
+                    <textarea name="content" placeholder="お問い合わせ内容をご記載ください" value="{{ old('content') }}"></textarea>
+                </div>
+                <div class="form__error">
+                    @error('content')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button--submit">確認画面</button>
         </div>
     </form>
 </div>
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FashionablyLate</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
     @yield('css')
@@ -17,10 +20,10 @@
             FashionablyLate
             </a>
         </div>
-        <!-- 20250526_0039管理画面などのloginボタンの装飾できないかと思って付け足してみたが、これだとすべての画面にでちゃう？？ -->
+        <!-- 20250526_0039管理画面などのloginボタンの装飾できないかと思って付け足してみたが、これだとすべての画面にでちゃう？？cssの設定すると画面上に出てきてしまうのでうまくいかず、いったんコメントアウトして保留
         <div class="header__button">
             @yield('button')
-        </div>
+        </div> -->
     </header>
 
     <main>
